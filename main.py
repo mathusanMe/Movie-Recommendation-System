38eca4382ee924fb8f6865af737bbd85fc289a76
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-02-22 15:36:57 +0100

    feat: Add KNN model and fitted the model to user_item matrix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
 
 # Load the data
 user_ratings_df = pd.read_csv('data/ratings.csv')
@@ -14,4 +15,8 @@ movie_data = user_ratings_df.merge(movie_metadata, on='movieId')
 # Create a user-item matrix
 user_item_matrix = user_ratings_df.pivot(index=['userId'], columns=['movieId'], values='rating').fillna(0)
 
-print(user_item_matrix.head())
+# Create a KNN model
+cf_knn_model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=10, n_jobs=-1)
+
+# Fit the model
+cf_knn_model.fit(user_item_matrix)
