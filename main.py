cde38aa66063a39fea1b84aa4115959d720feeb8
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-02-22 15:21:44 +0100

    feat: Merged user_ratings and movie_metadata

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 # Load the data
 user_ratings_df = pd.read_csv('data/ratings.csv')
 
-movie_metadata_df = pd.read_csv('data/movies_metadata.csv')
-movie_metadata_df = movie_metadata_df[['title', 'genres']]
-print(movie_metadata_df.head())
+movie_metadata = pd.read_csv('data/movies_metadata.csv')
+
+# Merge the data
+movie_data = user_ratings_df.merge(movie_metadata, on='movieId')
+print(movie_data.head())
